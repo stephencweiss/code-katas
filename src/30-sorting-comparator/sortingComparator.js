3c76e5ddf20857738f2e3a135b1ea22de9b505e5
Stephen <stephencweiss@gmail.com>
2020-02-23 09:34:20 -0600

    feat: read data successfully

--- src/30-sorting-comparator/sortingComparator.js
+++ src/30-sorting-comparator/sortingComparator.js
@@ -1,9 +1,31 @@
 const fs = require('fs')
 
-fs.readFile('/Users/stephen/_coding/personal/katas/hackerRankChallenges/src/30-sorting-comparator/sampleInput', {encoding: 'utf8'}, (err, data) => {
-    if (err) { throw new Error(`Failed to read`)}
-    console.log({data})
-})
+/**
+ * I: data: array of data where all values are space separated [name value]
+ * O: An array of tuples - of the space separated values
+ */
+function sortInput(data) {
+  return
+}
 
+function readInput(filePath) {
+  return new Promise((resolve, reject) =>
+    fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
+      if (err) {
+        reject(new Error(`Failed to read`))
+      }
+      const arr = data.split('\n')
+      arr.shift()
+      console.log({ data, arr })
+      resolve(arr)
+    })
+  )
+}
 
+function main() {
+  return readInput(
+    '/Users/stephen/_coding/personal/katas/hackerRankChallenges/src/30-sorting-comparator/sampleInput'
+  ).then(res => sortInput(res))
+}
 
+main()
