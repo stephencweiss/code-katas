fc3d20d295a468d3f89d0d34f723547db8557ca9
Stephen <stephen.c.weiss@gmail.com>
2020-03-13 15:22:42 -0500

    feat: find the duplicates

---
+++ src/39-find-the-duplicates/findTheDuplicates.js
@@ -0,0 +1,50 @@
+/**
+ *
+ * @param {Array} arr1 - sorted array in ascending order
+ * @param {Array} arr2 - sorted array in ascending order
+ */
+function findTheDuplicates(arr1, arr2) {
+  let duplicates = []
+  if (!arr1 || !arr2) return duplicates
+  let shortArr, longArr
+  if (arr1.length <= arr2.length) { // Ot(c), Os(c)
+    shortArr = arr1 
+    longArr = arr2  
+  } else {
+    shortArr = arr2
+    longArr = arr1
+  }
+
+  // Ot(c), Os(c)
+  let curShortIndex = 0
+  let curLongIndex = 0
+  let curShortVal = shortArr[curShortIndex] 
+  let curLongVal = longArr[curLongIndex]
+  // while there the short array element is defined
+  
+  //Ot(n+m) - worst case is to iterate over item in both lists, 
+  while (curShortVal && curLongVal) {
+      console.log({curLongIndex, curLongVal, curShortIndex, curShortVal})
+    // 1. the same as the element in the longer array - add to dupes list, increment both indices
+    if (curShortVal == curLongVal) {
+      duplicates.push(curLongVal) //Os(min(n,m)) - potentially every element in the smaller array needs to be stored
+      curShortIndex += 1
+      curLongIndex += 1
+      curShortVal = shortArr[curShortIndex] ? shortArr[curShortIndex] : undefined
+      curLongVal = longArr[curLongIndex]
+    }
+    // 2. less than the element in the longer array's element - increment just the shorter element
+    if (curShortVal < curLongVal) {
+      curShortIndex += 1
+      curShortVal = shortArr[curShortIndex] ? shortArr[curShortIndex] : undefined
+    }
+    // 3. greater than the element in the longer array's element - increment the longer lists' element
+    if (curShortVal > curLongVal) {
+        curLongIndex += 1
+        curLongVal = longArr[curLongIndex] ? longArr[curLongIndex] : undefined
+    }
+  }
+  return duplicates
+}
+
+module.exports = { findTheDuplicates }
---
+++ src/39-find-the-duplicates/findTheDuplicates.test.js
@@ -0,0 +1,27 @@
+const { findTheDuplicates } = require('./findTheDuplicates')
+
+test('expect findTheDuplicates to return an array of duplicates', () => {
+  const arr1 = [1, 2, 3, 5, 6, 7]
+  const arr2 = [3, 6, 7, 8, 20]
+  expect(findTheDuplicates(arr1, arr2)).toEqual([3, 6, 7])
+})
+test('expect findTheDuplicates to handle different starting positions', () => {
+  const arr1 = [1, 2, 3, 5, 6, 7, 20]
+  const arr2 = [8, 20]
+  expect(findTheDuplicates(arr1, arr2)).toEqual([20])
+})
+test('expect findTheDuplicates to handle shorter array spanning wider ranger', () => {
+  const arr1 = [2, 3, 5, 6, 7, 15]
+  const arr2 = [1, 7, 20]
+  expect(findTheDuplicates(arr1, arr2)).toEqual([7])
+})
+test('expect findTheDuplicates to return an empty array if no duplicates', () => {
+  const arr1 = [1, 2, 3]
+  const arr2 = [6, 7, 8, 20]
+  expect(findTheDuplicates(arr1, arr2)).toEqual([])
+})
+test('expect findTheDuplicates to handle undefined and null arguments', () => {
+  const arr1 = [1, 2, 3]
+  const arr2 = null
+  expect(findTheDuplicates(arr1, arr2)).toEqual([])
+})
