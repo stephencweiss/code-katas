44a33224affccd19011cbf6720372bdcd89cc165
Stephen <stephen.c.weiss@gmail.com>
2020-03-16 09:45:49 -0500

    fix: remove console.log

--- src/39-find-the-duplicates/findTheDuplicates.js
+++ src/39-find-the-duplicates/findTheDuplicates.js
@@ -8,8 +8,8 @@ function findTheDuplicates(arr1, arr2) {
   if (!arr1 || !arr2) return duplicates
   let shortArr, longArr
   if (arr1.length <= arr2.length) { // Ot(c), Os(c)
-    shortArr = arr1 
-    longArr = arr2  
+    shortArr = arr1
+    longArr = arr2
   } else {
     shortArr = arr2
     longArr = arr1
@@ -18,13 +18,12 @@ function findTheDuplicates(arr1, arr2) {
   // Ot(c), Os(c)
   let curShortIndex = 0
   let curLongIndex = 0
-  let curShortVal = shortArr[curShortIndex] 
+  let curShortVal = shortArr[curShortIndex]
   let curLongVal = longArr[curLongIndex]
   // while there the short array element is defined
-  
-  //Ot(n+m) - worst case is to iterate over item in both lists, 
+
+  //Ot(n+m) - worst case is to iterate over item in both lists,
   while (curShortVal && curLongVal) {
-      console.log({curLongIndex, curLongVal, curShortIndex, curShortVal})
     // 1. the same as the element in the longer array - add to dupes list, increment both indices
     if (curShortVal == curLongVal) {
       duplicates.push(curLongVal) //Os(min(n,m)) - potentially every element in the smaller array needs to be stored
