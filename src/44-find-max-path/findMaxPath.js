b073c0617c916c742c98b0da8546bdb6f5f43de2
Stephen <stephen.c.weiss@gmail.com>
2020-03-17 18:16:14 -0500

    refactor: clean up top function for readability

--- src/44-find-max-path/findMaxPath.js
+++ src/44-find-max-path/findMaxPath.js
@@ -2,73 +2,7 @@ export function findMaxPath(board) {
     let globalMaxPath = Number.NEGATIVE_INFINITY
     for (let row = 0; row < board.length; row += 1) {
         for (let column = 0; column < board[0].length; column += 1) {
-            function findValidPaths(
-                rowPosition,
-                columnPosition,
-                board,
-                pathTracker = [],
-                previousMove = 'NONE'
-            ) {
-                pathTracker.push(board[rowPosition][columnPosition])
-
-                if (pathTracker && pathTracker.length === 4) {
-                    // use lexical scoping to reach outside and add path to validPaths
-                    return validPaths.push(Number(pathTracker.join('')))
-                }
-
-                // look left
-                if (
-                    columnPosition - 1 >= 0 &&
-                    isValidMove(previousMove, 'LEFT')
-                ) {
-                    findValidPaths(
-                        rowPosition,
-                        columnPosition - 1,
-                        board,
-                        [...pathTracker],
-                        'LEFT'
-                    )
-                }
-                // look right
-                if (
-                    columnPosition + 1 < board[0].length &&
-                    isValidMove(previousMove, 'RIGHT')
-                ) {
-                    findValidPaths(
-                        rowPosition,
-                        columnPosition + 1,
-                        board,
-                        [...pathTracker],
-                        'RIGHT'
-                    )
-                }
-                // look up
-                if (rowPosition - 1 >= 0 && isValidMove(previousMove, 'UP')) {
-                    findValidPaths(
-                        rowPosition - 1,
-                        columnPosition,
-                        board,
-                        [...pathTracker],
-                        'UP'
-                    )
-                }
-                // look down
-                if (
-                    rowPosition + 1 < board.length &&
-                    isValidMove(previousMove, 'DOWN')
-                ) {
-                    findValidPaths(
-                        rowPosition + 1,
-                        columnPosition,
-                        board,
-                        [...pathTracker],
-                        'DOWN'
-                    )
-                }
-            }
-
-            const validPaths = []
-            findValidPaths(row, column, board)
+            const validPaths = findAllValidPaths(row, column, board)
             validPaths.sort()
             const largestValidPath = validPaths[validPaths.length - 1]
             if (largestValidPath > globalMaxPath)
@@ -76,6 +10,79 @@ export function findMaxPath(board) {
         }
     }
     return globalMaxPath
+}
+
+function findAllValidPaths(row, column, board) {
+    function findValidPaths(
+        rowPosition,
+        columnPosition,
+        board,
+        pathTracker = [],
+        previousMove = 'NONE'
+    ) {
+        pathTracker.push(board[rowPosition][columnPosition])
+
+        if (isValidPath(pathTracker)) {
+            // use lexical scoping to reach outside and add path to validPaths
+            return validPaths.push(Number(pathTracker.join('')))
+        }
+
+        // look left
+        if (columnPosition - 1 >= 0 && isValidMove(previousMove, 'LEFT')) {
+            findValidPaths(
+                rowPosition,
+                columnPosition - 1,
+                board,
+                [...pathTracker],
+                'LEFT'
+            )
+        }
+        // look right
+        if (
+            columnPosition + 1 < board[0].length &&
+            isValidMove(previousMove, 'RIGHT')
+        ) {
+            findValidPaths(
+                rowPosition,
+                columnPosition + 1,
+                board,
+                [...pathTracker],
+                'RIGHT'
+            )
+        }
+        // look up
+        if (rowPosition - 1 >= 0 && isValidMove(previousMove, 'UP')) {
+            findValidPaths(
+                rowPosition - 1,
+                columnPosition,
+                board,
+                [...pathTracker],
+                'UP'
+            )
+        }
+        // look down
+        if (
+            rowPosition + 1 < board.length &&
+            isValidMove(previousMove, 'DOWN')
+        ) {
+            findValidPaths(
+                rowPosition + 1,
+                columnPosition,
+                board,
+                [...pathTracker],
+                'DOWN'
+            )
+        }
+    }
+
+    const validPaths = []
+    findValidPaths(row, column, board)
+    return validPaths
+}
+
+function isValidPath(path) {
+    if (path && path.length === 4) return true
+    return false
 }
 
 const oppositeMoves = {
@@ -86,8 +93,6 @@ const oppositeMoves = {
 }
 /**
  * Determines if a move is valid based on the previous move; this works because we are limited to only four integers and as a result _cannot_ repeat unless we go back as diagonal values are not available.
- * @param {*} previousMove
- * @param {*} proposedMove
  */
 function isValidMove(previousMove, proposedMove) {
     return oppositeMoves[previousMove] !== proposedMove
