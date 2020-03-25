b67f35ee10b3a07e313d036d98d0a3f2cf316eff
Stephen <stephen.c.weiss@gmail.com>
2020-03-25 09:09:24 -0500

    refactor: remove redundancy

--- src/49-merge-sorted-arrays/mergeSortedArrays.js
+++ src/49-merge-sorted-arrays/mergeSortedArrays.js
@@ -2,29 +2,18 @@ function mergeSortedArrays(arrayA, arrayB) {
     let indexA = 0
     let indexB = 0
     let mergeSorted = []
-    while (indexA < arrayA.length && indexB < arrayB.length) {
-        if (arrayA[indexA] <= arrayB[indexB]) {
+    while (mergeSorted.length < arrayA.length + arrayB.length) {
+        const aIsExhausted = indexA >= arrayA.length
+        const bIsExhausted = indexB >= arrayB.length
+        if (!aIsExhausted && (bIsExhausted || arrayA[indexA] <= arrayB[indexB])) {
             mergeSorted.push(arrayA[indexA])
             indexA++
-        } else {
+        } else{
             mergeSorted.push(arrayB[indexB])
             indexB++
         }
     }
-
-    if (indexA < arrayA.length) {
-        for (indexA; indexA < arrayA.length; indexA++) {
-            mergeSorted.push(arrayA[indexA])
-        }
-    }
-
-    if (indexB < arrayB.length) {
-        for (indexB; indexB < arrayB.length; indexB++) {
-            mergeSorted.push(arrayB[indexB])
-        }
-    }
-
     return mergeSorted
 }
 
-module.exports = {mergeSortedArrays}
+module.exports = { mergeSortedArrays }
