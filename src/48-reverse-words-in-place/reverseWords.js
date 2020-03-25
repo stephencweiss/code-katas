b7541c734952205990f47ac3137ccb7c76f1b733
Stephen <stephen.c.weiss@gmail.com>
2020-03-25 08:52:34 -0500

    refactor: cleanup reverse words

--- src/48-reverse-words-in-place/reverseWords.js
+++ src/48-reverse-words-in-place/reverseWords.js
@@ -2,21 +2,15 @@ const { reverseCharactersInPlace } = require('../47-reverse-string-in-place/reve
 
 function reverseWords(arrayOfCharacters) {
     reverseCharactersInPlace(arrayOfCharacters)
-    // once the entire string is reversed
-    // the words will be in order, but the characters of each word will be reversed
     let wordStartInd = 0
     let wordEndInd = 0
     while (wordStartInd <= arrayOfCharacters.length) {
-        // starting with the 0th character, increment an end until you hit a space character - or the last character
         if ((arrayOfCharacters[wordEndInd] === ' ') || wordEndInd === arrayOfCharacters.length) {
-            // reverse that portion of the array using the same technique of reversing in place
-            // reverse subarray in place
             reverseWord(
                 arrayOfCharacters,
                 wordStartInd,
                 wordEndInd - 1 // go back one character from the space
             )
-            // reset start and end
             wordEndInd += 1
             wordStartInd = wordEndInd
         } else {
@@ -31,23 +25,3 @@ function reverseWord(arrayOfCharacters, wordStart, wordEnd) {
 }
 
 module.exports = { reverseWords }
-
-const message = [
-    'c',
-    'a',
-    'k',
-    'e',
-    ' ',
-    'p',
-    'o',
-    'u',
-    'n',
-    'd',
-    ' ',
-    's',
-    't',
-    'e',
-    'a',
-    'l',
-]
-reverseWords(message)
