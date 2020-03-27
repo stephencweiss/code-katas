67be811790a91073336eb8d234e773e794bf127a
Stephen <stephen.c.weiss@gmail.com>
2020-03-27 17:13:01 -0500

    refactor: use sets to optimize further
    by switching to sets from the objects we can take advantage of the
    constraint that we are only concerned with maximum of pairs.
    
    we can also use a for/of loop to avoid an extra o(n) operation to turn
    the string into an array.
    
    we don't need the conditional at the beginning for the "type" because
    if there's a character remaining in the set, it _implies_ only one "odd"
    occurrence.

--- src/52-palindrome-permutation/permutationsIncludePalindrome.js
+++ src/52-palindrome-permutation/permutationsIncludePalindrome.js
@@ -1,32 +1,17 @@
 // edge cases - case, i.e. A vs a
 
 function permutationsIncludePalindrome(str) {
-    const letters = str.split('')
-    const stringType = letters.length % 2 === 0 ? 'EVEN' : 'ODD'
-    const occurrences = {}
+    const unpairedLetters = new Set()
 
-    letters.forEach(letter => {
-        //O(n)
-        if (occurrences[letter]) {
-            occurrences[letter] += 1
+    for(let letter of str){
+        if (unpairedLetters.has(letter)) {
+            unpairedLetters.delete(letter)
         } else {
-            occurrences[letter] = 1
+            unpairedLetters.add(letter)
         }
-    })
+    }
 
-    const occurrencesCount = Object.values(occurrences)
-    let oddsIncluded = 0
-
-    occurrencesCount.forEach(occurrence => {
-        // O(c) where c is capped at ~26 for English (+/- based on how we want to handle other characters, but still it's capped at the types of characters)
-        if (occurrence % 2 !== 0) {
-            oddsIncluded += 1
-        }
-    })
-
-    if (stringType === 'EVEN') return true
-    else if ( stringType === 'ODD' && oddsIncluded === 1) return true
-    else return false
+    return unpairedLetters.size <= 1
 }
 
 module.exports = { permutationsIncludePalindrome }
