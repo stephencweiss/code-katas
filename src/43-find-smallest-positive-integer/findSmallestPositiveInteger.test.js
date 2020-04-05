42e24dfd8254119ac7ad440763b70c70255d0030
Stephen <stephen.c.weiss@gmail.com>
2020-04-05 16:13:39 -0500

    fix: test was set to only

--- src/43-find-smallest-positive-integer/findSmallestPositiveInteger.test.js
+++ src/43-find-smallest-positive-integer/findSmallestPositiveInteger.test.js
@@ -8,7 +8,7 @@ test('expect to handle negative numbers', () => {
     const arr = [-1,2,3,4,5]
     expect(findSmallestPositiveInteger(arr)).toBe(1)
 })
-test.only('expect to handle unsorted numbers, and double digit numbers', () => {
+test('expect to handle unsorted numbers, and double digit numbers', () => {
     const arr = [10,1,5,2,3,4,5]
     expect(findSmallestPositiveInteger(arr)).toBe(6)
 })
