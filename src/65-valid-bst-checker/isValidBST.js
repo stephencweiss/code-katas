7d8bd83548d88e988d85f7eccc9dfe67f822ac42
Stephen <stephen.c.weiss@gmail.com>
2020-04-14 11:21:57 -0500

    fix: handle internal tree
    the original implementation only conducted a check at the node/children
    level.
    
    this new approach inverts the level of interest to the node's ancestors.
    
    by looking above the node, I can confirm that each node's value is
    within the correct range, something not possible to do when lookign
    down in a bst.

--- src/65-valid-bst-checker/isValidBST.js
+++ src/65-valid-bst-checker/isValidBST.js
@@ -1,74 +1,29 @@
-class BinaryTreeNode {
-    constructor(value) {
-        this.value = value
-        this.left = null
-        this.right = null
-    }
-    printValue() {
-        console.log(this.value)
-    }
-    insertLeft(value) {
-        this.left = new BinaryTreeNode(value)
-        return this.left
-    }
+// the insight was to stop evaluating a node's children, but to evaluate the node itself
+// when looking at the children, my universe is very small (just the node and its children)
+// by evaluating the node itself, we can more easily keep track of its ancestors
 
-    insertRight(value) {
-        this.right = new BinaryTreeNode(value)
-        return this.right
-    }
-}
+// second insight was that when I go left, i reset the _upper_ bound
+// and go right, reset the _lower_ bound. I had this backwards for a long time
 
 function isValidBST(node) {
-    if (!node.left && !node.right) return true
-    if (!childrenAreValid(node)) return false
-    const queue = []
-    node.left && queue.push(node.left)
-    node.right && queue.push(node.right)
-    while (queue.length) {
-        const currentNode = queue.shift()
-        if (!childrenAreValid(currentNode)) return false
-        currentNode.left && queue.push(currentNode.left)
-        currentNode.right && queue.push(currentNode.right)
+    let isValid = true
+    function evaluateNode(node, lowerBound, upperBound) {
+        if (!isValidDescendant(node.value, lowerBound, upperBound)) {
+            console.log({ node, lowerBound, upperBound })
+            isValid = false
+            return isValid
+        }
+        isValid && node.left && evaluateNode(node.left, lowerBound, node.value)
+        isValid && node.right && evaluateNode(node.right, node.value, upperBound)
     }
-    return true
+
+    isValid && node.left && evaluateNode(node.left, Number.NEGATIVE_INFINITY, node.value)
+    isValid && node.right && evaluateNode(node.right, node.value, Number.POSITIVE_INFINITY)
+    return isValid
 }
 
-function childrenAreValid(node) {
-    const leftValid = node.left ? node.left.value < node.value : true
-    const rightValid = node.right ? node.value < node.right.value : true
-    return leftValid && rightValid
+function isValidDescendant(value, lowerBound, upperBound) {
+    return lowerBound <= value && value <= upperBound
 }
 
-module.exports = {isValidBST}
-
-const rootNode = new BinaryTreeNode(8)
-const node1 = new BinaryTreeNode(4)
-const node2 = new BinaryTreeNode(12)
-const node3 = new BinaryTreeNode(2)
-const node4 = new BinaryTreeNode(6)
-const node5 = new BinaryTreeNode(10)
-const node6 = new BinaryTreeNode(14)
-const node7 = new BinaryTreeNode(1)
-const node8 = new BinaryTreeNode(3)
-const node9 = new BinaryTreeNode(5)
-const node10 = new BinaryTreeNode(7)
-const node11 = new BinaryTreeNode(9)
-const node12 = new BinaryTreeNode(11)
-const node13 = new BinaryTreeNode(13)
-const node14 = new BinaryTreeNode(15)
-rootNode.left = node1
-rootNode.right = node2
-node1.left = node3
-node1.right = node4
-node2.left = node5
-node2.right = node6
-node3.left = node7
-node3.right = node8
-node4.left = node9
-node4.right = node10
-node5.left = node11
-node5.right = node12
-node6.left = node13
-node6.right = node14
-
-isValidBST(rootNode)
+module.exports = { isValidBST }
--- src/65-valid-bst-checker/isValidBST.test.js
+++ src/65-valid-bst-checker/isValidBST.test.js
@@ -52,6 +52,38 @@ describe('isValidBST', () => {
         node6.right = node14
         expect(isValidBST(rootNode)).toBe(true)
     })
+    test('invalid bst with extralarge child', () => {
+        const rootNode = new BinaryTreeNode(8)
+        const node1 = new BinaryTreeNode(4)
+        const node2 = new BinaryTreeNode(12)
+        const node3 = new BinaryTreeNode(2)
+        const node4 = new BinaryTreeNode(6)
+        const node5 = new BinaryTreeNode(10)
+        const node6 = new BinaryTreeNode(14)
+        const node7 = new BinaryTreeNode(1)
+        const node8 = new BinaryTreeNode(3)
+        const node9 = new BinaryTreeNode(5)
+        const node10 = new BinaryTreeNode(20)
+        const node11 = new BinaryTreeNode(9)
+        const node12 = new BinaryTreeNode(11)
+        const node13 = new BinaryTreeNode(13)
+        const node14 = new BinaryTreeNode(15)
+        rootNode.left = node1
+        rootNode.right = node2
+        node1.left = node3
+        node1.right = node4
+        node2.left = node5
+        node2.right = node6
+        node3.left = node7
+        node3.right = node8
+        node4.left = node9
+        node4.right = node10
+        node5.left = node11
+        node5.right = node12
+        node6.left = node13
+        node6.right = node14
+        expect(isValidBST(rootNode)).toBe(false)
+    })
     test('invalid bst', () => {
         const rootNode = new BinaryTreeNode(3)
         const node1 = new BinaryTreeNode(4)
