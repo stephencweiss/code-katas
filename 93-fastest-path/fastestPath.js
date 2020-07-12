2daf3cb425990de3ccfd8480dabcc4005079c472
Stephen <stephen.c.weiss@gmail.com>
2020-07-12 08:23:02 -0700

    feat: improved fastestPath
    by taking advantage of the fact that the maximum horizontal or vertical
    distance will be the longest time, we can use a simpler calculation by
    leveraging the the absolute value to find the distance.
    
    now, instead of incremental steps, we can go directly to the time it
    takes to traverse the distance.

--- 93-fastest-path/fastestPath.js
+++ 93-fastest-path/fastestPath.js
@@ -3,50 +3,14 @@
  * @return {number}
  */
 var minTimeToVisitAllPoints = function(points) {
-    // keep track of current time elapsed
     let timeElapsed = 0
-    // set current position = the first position in points
-    let currentPosition = points[0]
-    // for each point
-    for (let i = 0; i < points.length; i++) {
-        const targetPosition = points[i]
-        let moveNecessary = true
-        while (moveNecessary) {
-            if (
-                currentPosition[0] == targetPosition[0] &&
-                currentPosition[1] == targetPosition[1]
-            ) {
-                currentPosition = targetPosition
-                moveNecessary = false
-            } else {
-                currentPosition = movePoint(currentPosition, targetPosition)
-                timeElapsed += 1
-            }
-        }
+    for (let i = 0; i < points.length - 1; i++) {
+        let [xCurrent, yCurrent] = points[i]
+        const [xTarget, yTarget] = points[i + 1]
+        timeElapsed += Math.max(
+            Math.abs(xCurrent - xTarget),
+            Math.abs(yCurrent - yTarget)
+        )
     }
     return timeElapsed
-}
-
-/**
- * Takes a current and target point,
- * returns a newCurrent
- */
-function movePoint(current, target) {
-    let [xCurrent, yCurrent] = current
-    const [xTarget, yTarget] = target
-
-    if (xCurrent < xTarget) {
-        xCurrent += 1
-    }
-    if (yCurrent < yTarget) {
-        yCurrent += 1
-    }
-    if (xTarget < xCurrent) {
-        xCurrent -= 1
-    }
-    if (yTarget < yCurrent) {
-        yCurrent -= 1
-    }
-    console.log({ xCurrent, yCurrent })
-    return [xCurrent, yCurrent]
 }
